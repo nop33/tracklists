ce0b29bb37616552e19528ed1227a31578c97d14
Ilias Trichopoulos <hlias.nop@gmail.com>
2020-06-25 16:23:28 +0200

    Upload and display iTunes tracks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-file-input
+            label="Upload iTunes playlist file"
+            @change="fileChanged"
+          ></v-file-input>
+          <v-card
+            tile
+          >
+            <v-list-item two-line v-for="(track, index) in tracks" :key="`${track['Name']}-${index}`">
+              <v-list-item-content>
+                <v-list-item-title>{{ track['Name'] }}</v-list-item-title>
+                <v-list-item-subtitle>{{ track['Artist'] }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-col>
+        <v-col>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+    reader: new FileReader(),
+    tracks: []
+  }),
+  methods: {
+    fileChanged (file) {
+      this.reader.readAsText(file)
+    },
+    processITunesLibraryFile (file) {
+      const lines = file.split(/[\r\n]+/)
+      const trackProperties = lines.shift().split('\t')
+      lines.pop() // remove empty last line
+
+      lines.forEach(line => {
+        const trackData = line.split('\t')
+        const track = {}
+        trackData.forEach((trackPropertyValue, index) => {
+          track[trackProperties[index]] = trackPropertyValue
+        })
+        this.tracks.push(track)
+      })
+    }
+  },
+  created () {
+    this.reader.addEventListener('load', (event) => {
+      const file = event.target.result
+      this.processITunesLibraryFile(file)
+    })
   }
 }
 </script>
